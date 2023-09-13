c87559a791ede502365797f1532ef64a12aa6d8c
NathanDavids <129671319+NathanDavids@users.noreply.github.com>
2023-09-13 11:36:16 +0200

    Added search functionality

--- src/stores/homeStore.js
+++ src/stores/homeStore.js
@@ -1,8 +1,36 @@
 import axios from 'axios'
 import { create } from 'zustand'
+import debounce from '../helpers/debounce'
 
 const homeStore = create((set) => ({
     coins: [],
+    trending: [],
+    query: '',
+
+    setQuery: (e) => {
+        set({query: e.target.value})
+        homeStore.getState().searchCoins()
+    },
+
+    searchCoins: debounce (async () => {
+        const {query, trending} = homeStore.getState()
+
+        if (query.length > 2) {
+        const res = await axios.get(`https://api.coingecko.com/api/v3/search?query=${query}`)
+        
+        const coins = res.data.coins.map(coin => {
+            return{
+                name: coin.name, 
+                image: coin.large, 
+                id: coin.id
+            }
+        })
+
+        set({coins})
+        } else {
+            set({ coins: trending })
+        }
+    }, 500),
   
     fetchCoins: async () => {
     const res = await axios.get('https://api.coingecko.com/api/v3/search/trending')
@@ -16,7 +44,7 @@ const homeStore = create((set) => ({
         }
     })
 
-    set({coins})
+    set({coins, trending: coins})
   }
 }))
 
