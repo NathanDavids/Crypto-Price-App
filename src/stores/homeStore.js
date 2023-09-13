595738d29fc8816dea5802a4b908d6357ee8830f
NathanDavids <129671319+NathanDavids@users.noreply.github.com>
2023-09-13 10:35:46 +0200

    Created the main components

---
+++ src/stores/homeStore.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+import { create } from 'zustand'
+
+const homeStore = create((set) => ({
+    coins: [],
+  
+    fetchCoins: async () => {
+    const res = await axios.get('https://api.coingecko.com/api/v3/search/trending')
+    
+    const coins = res.data.coins.map(coins => {
+        return{
+            name: coins.item.name, 
+            image: coins.item.large,
+            id: coins.item.id,
+            priceBtc: coins.item.price_btc
+        }
+    })
+
+    set({coins})
+  }
+}))
+
+export default homeStore
